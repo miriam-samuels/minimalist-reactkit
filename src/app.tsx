c7af5acc75f2a31a7b3dd3cba654a16efccd23fb
miriam-samuels <medomiriam@gmail.com>
2023-09-20 23:55:23 +0100

    Switch to app structure

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-    
+    <h1>Hello</h1>
     </div>
   )
 }
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "./app";
+import ReactDOM from 'react-dom/client';
+
+
+ReactDOM.createRoot(
+   document.getElementById('root') as HTMLElement
+).render(
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>
+);
